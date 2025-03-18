48b43bfa4e9e24299c0fdee663c4da448a906529
andreyn25 <andryn25@bergen.org>
2025-03-17 22:20:37 -0400

    Update maze.java

--- maze.java
+++ maze.java
@@ -21,40 +21,21 @@ public class maze {
             }
         }
 
-        //Initialize corners
-        maze[0][0].below = Math.random();
-        maze[0][0].right = Math.random();
-        maze[0][n-1].below = Math.random();
-        maze[0][n-1].left = Math.random();
-        maze[n-1][0].above = Math.random();
-        maze[n-1][0].right = Math.random();
-        maze[n-1][n-1].above = Math.random();
-        maze[n-1][n-1].left = Math.random();
-
-        //Initialize sides of maze
-        for (int i = 1; i < n-1; i++) {
-            maze[0][i].left = Math.random();
-            maze[0][i].below = Math.random();
-            maze[0][i].right = Math.random();
-            maze[n-1][i].left = Math.random();
-            maze[n-1][i].above = Math.random();
-            maze[n-1][i].below = Math.random();
-            maze[i][0].above = Math.random();
-            maze[i][0].right = Math.random();
-            maze[i][0].below = Math.random();
-            maze[i][n-1].above = Math.random();
-            maze[i][n-1].left = Math.random();
-            maze[i][n-1].below = Math.random();
-        }
-
         //Initialize n-2 square
-        for (int i = 1; i < n-1; i++) {
-            for (int j = 1; j < n-1; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n; j++) {
                 maze[i][j].above = Math.random();
                 maze[i][j].below = Math.random();
                 maze[i][j].left = Math.random();
                 maze[i][j].right = Math.random();
             }
+        }
+
+        for (int i = 0; i < n; i++) {
+            maze[0][i].above = Double.MAX_VALUE;
+            maze[n-1][i].below = Double.MAX_VALUE;
+            maze[i][0].left = Double.MAX_VALUE;
+            maze[i][n-1].right = Double.MAX_VALUE;
         }
 
         //Now to initialize connections by choosing cheapest
